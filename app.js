75a26dc45d634fc6138cc37c7061084396a7ae20
chroaster <chroaster@pm.me>
2021-10-18 22:01:33 -0700

    content type json, passed error msg, revised comments

--- app.js
+++ app.js
@@ -13,22 +13,28 @@ app.get('/', async (req, res) => {
   // retrieve the url provided as a query parameter
   const url = req.query.url;
 
-  // "forward" the fetch request and return the response
-  try {
-    const response = await fetch(url);
-    if (response.ok) {
-      // add cors header for "wide open cors" (uncomment as needed)
-      res.header('Access-Control-Allow-Origin', '*');
-      // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  // set content type for json
+  res.set('Content-Type', 'application/json');
 
-      // send the response from the target with the added cors headers
+  try {
+    // "forward" the original request to the target and capture response
+    const response = await fetch(url);
+
+    if (response.ok) {
+      // add header(s) for "wide open cors" (uncomment as needed)
+      res.set({
+        'Access-Control-Allow-Origin': '*',
+        // 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+      });
+
+      // respond to original request with modified response from target
       res.send(await response.text());
     } else {
       throw new Error(response.statusText);
     }
   } catch (err) {
     // error retrieving resource, respond with request dependency error
-    res.status(424).send();
+    res.status(424).send(err);
   }
 });
 
